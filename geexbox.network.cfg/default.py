3c8174ec07ea0417a7c09b413c79f98f65c19c3f
warped-rudi <r.ihle@s-t.de>
2015-03-27 13:24:00 +0100

    initial import

---
+++ geexbox.network.cfg/default.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import xbmcaddon
+
+__scriptname__ = "GeeXboX network configuration"
+__author__ = "The Geexbox Team"
+__url__ = "http://www.geexbox.org"
+__version__ = "0.1.2"
+
+__settings__   = xbmcaddon.Addon(id='geexbox.addon.network')
+__language__   = __settings__.getLocalizedString
+__cwd__        = __settings__.getAddonInfo('path')
+
+ROOTDIR = os.getcwd().replace( ";", "" )
+
+def is_exe(fpath):
+  return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+def refreshSettings():
+  try:
+    f = open("/etc/network")
+  except Exception as e:
+    print "Error: cannot open /etc/network: %s" % e
+    return 'error'
+
+  for line in f:
+    line = line.strip()
+    if len(line) <= 0:
+      continue
+
+    if line[0] == '#':
+      continue
+
+    s = line.split('=', 1)
+    if len(s) != 2:
+      continue
+
+    # trim spurious whitespace, unquote value
+    s[1] = s[1].strip().strip('"')
+
+    __settings__.setSetting(s[0], s[1])
+
+def Main():
+  refreshSettings()
+  __settings__.openSettings()
+  if is_exe("/bin/update-config-network"):
+    execcmd("/bin/update-config-network")
+  elif is_exe("/usr/bin/update-config-network"):
+    execcmd("/usr/bin/update-config-network")
+  else:
+    print "Error: cannot find update-config-network!"
+    return 'error'
+ 
+def execcmd(cmd):
+  (child_stdin, child_stdout, child_stderr) = os.popen3(cmd)
+  stderr = child_stderr.readlines()
+  stdout = child_stdout.readlines()
+  child_stdin.close()
+  child_stdout.close()
+  child_stderr.close()
+  if stderr ==[]:
+    print " Cmd Ok result is %s" % stdout
+    return stdout
+  else:
+    print "Error: %s" % stderr
+    return 'error'
+
+
+if __name__ == "__main__":
+  Main()
