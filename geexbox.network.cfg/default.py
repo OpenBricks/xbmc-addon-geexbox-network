c4ab12fe2ba3300f485fa8919dd1d441168493cf
Rudi <r.ihle@s-t.de>
2016-06-24 17:12:02 +0200

    Add support for second interface, update script no longe needed

--- geexbox.network.cfg/default.py
+++ geexbox.network.cfg/default.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import time
+import hashlib
 import xbmcgui
 import xbmcaddon
 import xml.etree.ElementTree as ET
@@ -15,9 +17,11 @@ __cwd__        = __settings__.getAddonInfo('path')
 
 ROOTDIR = os.getcwd().replace( ";", "" )
 
+# check, if a file is executable
 def is_exe(fpath):
   return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
+# execute program and retrieve it's stdout as string list
 def execcmd(cmd):
   (child_stdin, child_stdout, child_stderr) = os.popen3(cmd)
   stderr = child_stderr.readlines()
@@ -29,35 +33,37 @@ def execcmd(cmd):
     print " Cmd Ok result is %s" % stdout
     return stdout
   print "Error: %s" % stderr
-  return 'error'
+  return []
 
-def loadNetworkConfig():
+# import settings from /etc/network(2), calculate checksum
+def loadNetworkConfig(cfgFile, suffix):
   try:
-    f = open("/etc/network")
+    f = open(cfgFile)
   except Exception as e:
-    print "Error: cannot open /etc/network: %s" % e
+    print "Error: cannot open %s: %s" % (cfgFile, e)
     return 'error'
 
+  h = hashlib.md5()
   for line in f:
+    s = []
     line = line.strip()
-    if len(line) <= 0:
-      continue
-    if line[0] == '#':
-      continue
-    s = line.split('=', 1)
+    h.update(line)
+
+    if len(line) > 0 and line[0] != '#':
+      s = line.split('=', 1)
     if len(s) != 2:
       continue
 
     # trim spurious whitespace, unquote value
     s[1] = s[1].strip().strip('"')
-    __settings__.setSetting(s[0], s[1])
+    __settings__.setSetting(s[0] + suffix, s[1])
 
     if s[0] != "ADDRESS":
       continue
 
     # ip address and net mask from 'slashed' form
     am = s[1].split('/', 1)
-    __settings__.setSetting("NETADDR", am[0])
+    __settings__.setSetting("NETADDR" + suffix, am[0])
 
     msk = ""
     if len(am) == 2:
@@ -66,22 +72,58 @@ def loadNetworkConfig():
       m = -256
     for i in (24,16,8,0):
       msk = msk + '.' + str((m >> i) & 255)
-    __settings__.setSetting("NETMASK", msk[1:])
+    __settings__.setSetting("NETMASK" + suffix, msk[1:])
 
   f.close
+  return h.hexdigest()
 
-def search_and_replace(label,values):
-  settings_file = __cwd__ + "/resources/settings.xml"
-  tree = ET.ElementTree(file=settings_file)
+# export settings to /etc/network(2), check for checksum change
+def saveNetworkConfig(cfgFile, suffix, chksum):
+  try:
+    f = open(cfgFile)
+  except Exception as e:
+    print "Error: cannot open %s: %s" % (cfgFile, e)
+    return 0
+
+  tmpFile = cfgFile + ".$$$"
+  o = open(tmpFile, "w")
+
+  h = hashlib.md5()
+  for line in f:
+    s = []
+    line = line.strip()
+    if len(line) > 0 and line[0] != '#':
+      s = line.split('=', 1)
+    if len(s) == 2:
+      line = "%s=\"%s\"" % (s[0], __settings__.getSetting(s[0] + suffix))
+
+    h.update(line)
+    o.write(line + '\n')
+
+  o.close()
+  f.close()
+
+  if h.hexdigest() == chksum:
+    os.remove(tmpFile)
+    return 0
+  
+  os.remove(cfgFile)
+  os.rename(tmpFile, cfgFile)
+  return 1
+
+# inject detected information into GUI *.xml
+def search_and_replace(guiFile, label, values):
+  tree = ET.ElementTree(file=guiFile)
   root = tree.getroot()
   for elem in tree.iter('setting'):
     if len(elem.attrib) > 1 and elem.attrib['label'] == label:
       elem.set('values', values)
-  tree.write(settings_file)
+  tree.write(guiFile)
 
+# convert string list to string using '|' as separator
 def prepare_data(data):
   if len(data) <= 0:
-    return ' '
+    return "-"
   a = data
   for i in range(len(a)):
     a[i] = str(a[i]).strip().strip('\'')
@@ -92,13 +134,35 @@ def prepare_data(data):
     b = b + '|' + a[i]
   return b
 
+# write ip address, optionally with net mask in slash notation
 def set_ip(label, value, suffix):
   if value == "" or value == "0.0.0.0":
     __settings__.setSetting(label, "")
   else:
     __settings__.setSetting(label, value + suffix)
 
+# restart network services
+def reload_network_backend(name):
+  restart_list = [ \
+    "telnetd.socket", "sshd.socket", \
+    name + ".service", "bftpd.service", \
+    "lighttpd.service", "smbd.service", "nmbd.service" ]
+
+  # always execute this first...
+  os.system("systemctl restart network-link.service")
+
+  args = ""
+  for unit in restart_list:
+    if os.access("/lib/systemd/system/" + unit, os.F_OK):
+      args = args + ' ' + unit 
+
+  if args != "":
+    time.sleep(1)
+    os.system("systemctl restart" + args)
+
+# main entry
 def Main():
+  # create progress dialog (special handling for Frodo)
   try:
     busy_dlg = xbmcgui.DialogProgress()
     busy_dlg.create(__scriptname__, __settings__.getLocalizedString(32300))
@@ -108,58 +172,66 @@ def Main():
     busy_dlg.show()
     may_cancel = 0
 
-  loadNetworkConfig()
+  chksum1 = loadNetworkConfig("/etc/network", "")
+  chksum2 = loadNetworkConfig("/etc/network2", "2")
+
   client = __settings__.getSetting("NETWORK_BACKEND")
+  __settings__.setSetting("NETWORK_BACKEND2", client)
 
   if client == "networkmanager":
     ssid_found = prepare_data(execcmd("nmcli -f SSID dev wifi | tail -n +2"))
     ifaces_found = prepare_data(execcmd("nmcli -f DEVICE dev | tail -n +2"))
   elif client == "connman":
-    execcmd("connmanctl enable wifi")
-    execcmd("connmanctl scan wifi")
+    os.system("connmanctl enable wifi; connmanctl scan wifi")
     ssid_found = prepare_data(execcmd("connmanctl services | grep wifi | sed -e 's/^.\{4\}//' -e 's/wifi_.*//'"))
     ifaces_found = prepare_data(execcmd("ifconfig -a | grep Ethernet | sed -e 's/ *Link.*//'"))
   else:
-    ssid_found = " "
-    ifaces_found = "eth0|eth1|wlan0"
+    ssid_found = "-"
+    ifaces_found = "-"
 
   if may_cancel and busy_dlg.iscanceled():
     return
 
-  search_and_replace('32020', ifaces_found)
-  search_and_replace('32062', ssid_found)
+  if ifaces_found == "-":
+    ifaces_found = "eth0|eth1|wlan0"
+
+  dialog_file = __cwd__ + "/resources/settings.xml"
+  search_and_replace(dialog_file, '32020', ifaces_found)
+  search_and_replace(dialog_file, '32120', ifaces_found)
+  search_and_replace(dialog_file, '32062', ssid_found)
+  search_and_replace(dialog_file, '32162', ssid_found)
 
   busy_dlg.close()
 
   __settings__.openSettings()
 
-  # retrieve detected SSID
-  ssid_temp = __settings__.getSetting("DETECTED_SSID")
-  auto_wifi = __settings__.getSetting("USE_DETECTED_SSID")
-  if auto_wifi == "true" and ssid_temp != "":
-    __settings__.setSetting("SSID", ssid_temp)
-  __settings__.setSetting("USE_DETECTED_SSID", "false")
+  for suffix in ("","2"):
+    # retrieve detected SSID
+    ssid_temp = __settings__.getSetting("DETECTED_SSID" + suffix)
+    auto_wifi = __settings__.getSetting("USE_DETECTED_SSID" + suffix)
+    if auto_wifi == "true" and ssid_temp != "-":
+      __settings__.setSetting("SSID" + suffix, ssid_temp)
+    __settings__.setSetting("USE_DETECTED_SSID" + suffix, "false")
 
-  # convert ip address and netmask to 'slash form'
-  adr = __settings__.getSetting("NETADDR")
-  msk = __settings__.getSetting("NETMASK").split('.', 4)
-  cnt = 0
-  for i in range(0,31):
-    if ((int(msk[i / 8]) << (i % 8)) & 128) == 0:
-      break
-    cnt = cnt + 1
-  set_ip("ADDRESS", adr, '/' + str(cnt))
-  set_ip("GATEWAY", __settings__.getSetting("GATEWAY"), "")
-  set_ip("DNS_SERVER", __settings__.getSetting("DNS_SERVER"), "")
+    # convert ip address and netmask to 'slash form'
+    adr = __settings__.getSetting("NETADDR" + suffix)
+    msk = __settings__.getSetting("NETMASK" + suffix).split('.', 4)
+    cnt = 0
+    for i in range(0,31):
+      if ((int(msk[i / 8]) << (i % 8)) & 128) == 0:
+        break
+      cnt = cnt + 1
 
+    set_ip("ADDRESS" + suffix, adr, '/' + str(cnt))
+    set_ip("GATEWAY" + suffix, __settings__.getSetting("GATEWAY" + suffix), "")
+    set_ip("DNS_SERVER" + suffix, __settings__.getSetting("DNS_SERVER" + suffix), "")
 
-  if is_exe("/bin/update-config-network"):
-    execcmd("/bin/update-config-network")
-  elif is_exe("/usr/bin/update-config-network"):
-    execcmd("/usr/bin/update-config-network")
-  else:
-    print "Error: cannot find update-config-network!"
-    return 'error'
+  changed = 0
+  changed = changed + saveNetworkConfig("/etc/network", "", chksum1)
+  changed = changed + saveNetworkConfig("/etc/network2", "2", chksum2)
+  
+  if changed != 0:
+    reload_network_backend(client)
 
 if __name__ == "__main__":
   Main()
